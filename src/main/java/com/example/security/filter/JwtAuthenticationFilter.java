441a03b9c38922c40f86485c5f2ce9deccb52245
haniyeyo <jh9434145@gmail.com>
2026-02-25 12:43:19 +0900

    radis 검증

--- src/main/java/com/example/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/example/security/filter/JwtAuthenticationFilter.java
@@ -2,11 +2,11 @@ package com.example.security.filter;
 
 import java.io.IOException;
 
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.util.PatternMatchUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import com.example.member.repository.MemberRepository;
 import com.example.security.JwtTokenProvider;
 
 import jakarta.servlet.FilterChain;
@@ -21,8 +21,8 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 	
-	private final MemberRepository memberRepository;
 	private final JwtTokenProvider jwtTokenProvider;
+	private final StringRedisTemplate redisTemplate;
     private static final String[] whiteList = {"/", "/event", "/member/*", "/dbtest", "/api/core/**", "/signup/*", "/signup.html","/api/**"};
     
 	@Override
@@ -54,13 +54,17 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         		throw new RuntimeException("유효하지 않은 토큰입니다.");
         	}
         	
-        	// DB에 있는지 확인
-        	String userId = jwtTokenProvider.getSubject(token);
-        	if(!memberRepository.existsById(Long.parseLong(userId))) {
-        		throw new RuntimeException("존재하지 않는 회원입니다.");
+        	// Redis에 토큰이 살아있는지 확인
+        	String redisKey = "TOKEN:" + token;
+        	String userInfo = redisTemplate.opsForValue().get(redisKey);
+        	if(userInfo == null) {
+        		log.error("---------> [AUTH] Redis에 토큰이 없음 (만료되었거나 로그아웃됨)");
+        		throw new RuntimeException("로그아웃되었거나 만료된 세션입니다.");
         	}
-        	
-        	log.info("---------> [인증성공] 유저 ID: {}", userId);
+        	// Redis에 값이 있다면 확인 가능
+        	log.info("---------> [AUTH] Redis 확인 완료: {}", userInfo);
+        
+        	log.info("---------> [인증성공] 유저 정보: {}", userInfo);
         	filterChain.doFilter(request, response);
         	
         } catch (Exception e) {
