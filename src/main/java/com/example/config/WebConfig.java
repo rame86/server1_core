155e0c18a033565002c3647717cfb41038997d34
rame86 <ssrabw86@gmail.com>
2026-03-11 15:45:44 +0900

    한글설정 강제

--- src/main/java/com/example/config/WebConfig.java
+++ src/main/java/com/example/config/WebConfig.java
@@ -1,14 +1,16 @@
 package com.example.config;
 
-import com.example.common.resolver.LoginUserArgumentResolver;
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean; // 추가
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.reactive.function.client.WebClient; // 추가
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration; // 추가
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer; // 추가
+
+import com.example.common.resolver.LoginUserArgumentResolver;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @RequiredArgsConstructor
@@ -16,6 +18,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     private final LoginUserArgumentResolver loginUserArgumentResolver;
 
+    
     // 이 부분을 추가하세요! 스프링이 WebClient를 주입해줄 수 있게 빈으로 등록합니다.
     @Bean
     public WebClient webClient(WebClient.Builder builder) {
