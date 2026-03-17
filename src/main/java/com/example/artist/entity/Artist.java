dd08299d8811d4095496b11ca404c704147ba781
haniyeyo <jh9434145@gmail.com>
2026-03-17 16:01:44 +0900

    imageUrl save

--- src/main/java/com/example/artist/entity/Artist.java
+++ src/main/java/com/example/artist/entity/Artist.java
@@ -32,5 +32,6 @@ public class Artist {
     private String communityLink;  // 아티스트별 커뮤니티 링크 (외부 링크)
     private String description;    // 소개글
     private String category;
+    private int followerCount = 0;
     
 }
