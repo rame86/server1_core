ece3f88a9de53d17c328a7b24864e3f226432f36
rame86 <ssrabw86@gmail.com>
2026-03-25 13:07:05 +0900

    아티스트 살려내

--- src/main/java/com/example/artist/entity/Artist.java
+++ src/main/java/com/example/artist/entity/Artist.java
@@ -20,24 +20,25 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class Artist {
-	
-	@Id
+
+    @Id
     private Long memberId;
-	
-	@MapsId // Artist의 PK를 Member의 PK와 매핑
+
+    @MapsId // Artist의 PK를 Member의 PK와 매핑
     @OneToOne(fetch = FetchType.LAZY) // 지연 로딩으로 성능 최적화
     @JoinColumn(name = "member_id")
     private Member member;
-	
-	private String stageName;      // 활동명
-    private String communityLink;  // 아티스트별 커뮤니티 링크 (외부 링크)
-    private String description;    // 소개글
+
+    private String stageName; // 활동명
+    private String communityLink; // 아티스트별 커뮤니티 링크 (외부 링크)
+    private String description; // 소개글
     private String category;
     private String profileImageUrl;
     private int followerCount = 0;
     private String fandomName;
-    
+
     // Artist.java
     @Column(name = "artist_id") // DB의 artist_id 컬럼과 매칭
     private Long artistId;
+
 }
