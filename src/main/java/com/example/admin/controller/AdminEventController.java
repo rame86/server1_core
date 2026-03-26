54cd23d627975277786e19073469d99ea76d0985
rame86 <ssrabw86@gmail.com>
2026-03-26 20:11:09 +0900

    어드민 이벤트

--- src/main/java/com/example/admin/controller/AdminEventController.java
+++ src/main/java/com/example/admin/controller/AdminEventController.java
@@ -22,7 +22,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
-@RequestMapping("/admin/event")
+@RequestMapping("/admin")
 @RequiredArgsConstructor
 public class AdminEventController {
 	
